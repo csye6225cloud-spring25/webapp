83c0ae4afa9002013f70c2ff8dd7ff047c0ede3b
Rahul Ramesh Patil <patil.rahu@northeastern.edu>
2025-03-16 12:35:15 -0400

    Updated app setup script

--- app-setup.sh
+++ app-setup.sh
@@ -6,17 +6,16 @@ echo "=== Setting up the application ==="
 # Create the app directory if it doesn't exist
 sudo mkdir -p /opt/app 
 
+sudo apt install -y unzip
+
 # Unzip the application artifact
-sudo unzip -o /tmp/backend.zip -d /opt/app/backend
+sudo unzip -o /tmp/backend.zip -d /opt/app/
 
 
 # This wont work now as we have to create the RDS and use that endpoint dynamically
 
 # Create the .env file dynamically with the DATABASE_URL passed from Packer 
 # echo "DATABASE_URL=${DATABASE_URL}" | sudo tee /opt/app/backend/.env
-
-# Ensure proper permissions for the .env file
-sudo chmod 600 /opt/app/backend/.env
 
 # Ensure csye6225 has a home directory and it's owned by csye6225
 sudo mkdir -p /home/csye6225
@@ -30,8 +29,8 @@ echo "=== Clearing npm cache ==="
 sudo -u csye6225 bash -c "cd /opt/app/backend && npm cache clean --force"
 
 # Remove node_modules and package-lock.json
-echo "=== Removing existing node_modules and package-lock.json ==="
-sudo -u csye6225 bash -c "cd /opt/app/backend && rm -rf node_modules package-lock.json"
+# echo "=== Removing existing node_modules and package-lock.json ==="
+# sudo -u csye6225 bash -c "cd /opt/app/backend && rm -rf node_modules package-lock.json"
 
 # Install Node.js dependencies
 echo "=== Installing Node.js dependencies ==="
